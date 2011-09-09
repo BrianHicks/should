ace8dee2669de765a705595964fa1e7dffd8d633
Brian Hicks <brian.hicks@rockfishinteractive.com>
2011-09-09 15:49:41 -0500

    get PyLint score of 10.0

--- should.py
+++ should.py
@@ -1,9 +1,6 @@
 'Should, a small command line app for working with plain text todo files'
 import argparse
 import os
-import re
-import sys
-from datetime import datetime
 
 ## COLORS ##
 
@@ -28,6 +25,7 @@ COLORS = {
 }
 
 def wrap_color(args, text, *colors):
+    'wrap text in a color defined in COLORS. If opts.color is "n", don\'t.'
     if args.color == 'n':
         return text
 
@@ -42,74 +40,82 @@ def wrap_color(args, text, *colors):
 ## METHODS ##
 
 def get_absolute_file_name(name):
+    'get the absolute file name relative to should.py'
     return os.path.join(os.path.dirname(__file__), '%s.txt' % name)
 
 def get_named_file_lines(name):
     'get the lines from a named file (assuming .txt extension)'
     try:
-        with open(get_absolute_file_name(name), 'rb') as f:
-            return [l for l in f.read().split('\n') if l != '']
+        with open(get_absolute_file_name(name), 'rb') as named_file:
+            return [line for line 
+                    in named_file.read().split('\n') 
+                    if line != '']
     except IOError:
         return []
 
 def write_named_file_lines(name, lines):
     'write lines from a list to a file'
     try:
-        with open(get_absolute_file_name(name), 'wb') as f:
+        with open(get_absolute_file_name(name), 'wb') as out_file:
             for line in lines:
-                f.write('%s\n' % line)
+                out_file.write('%s\n' % line)
     except IOError:
         print 'could not write lines to file, here they are:'
         print lines
 
 def format_verbose_line(args, i, text):
+    'format a line of text in a verbose format'
     return '%s: %s' % (wrap_color(args, str(i), 'blue'), text)
 
 def print_todos(args):
+    'print all todos in a verbose format'
     todos = get_named_file_lines('todo')
     for i, todo in enumerate(todos):
         print format_verbose_line(args, i, todo)
 
 def complete_todo(args):
+    'complete a todo specified in args.n'
     todos = get_named_file_lines('todo')
     for i, todo in enumerate(todos):
         if i == int(args.n):
-            archive_todo(todos[i])
+            archive_todo(todo)
             del todos[i]
 
     write_named_file_lines('todo', todos)
 
 def archive_todo(text):
+    'archive a line of text'
     todos = get_named_file_lines('archive')
     todos.append(text)
     write_named_file_lines('archive', todos)
 
 def add_todo(args):
+    'add a todo to the text file'
     todos = get_named_file_lines('todo')
     todos.append(args.text)
-    print 'adding todo',args.text
+    print 'adding todo:', args.text
     write_named_file_lines('todo', todos)
 
 def search_todos(args):
+    'search todos on a single matched string'
     todos = get_named_file_lines('todo')
     for i, todo in enumerate(todos):
         if todo.find(args.text) != -1:
             print format_verbose_line(args, i, todo)
 
-def refresh_projects():
-    print 'not fully functional.'
-    todos = get_named_file_lines('todo')
-    projects = {}
-    for todo in todos:
-        print todo
-
 ## EXECUTION ##
 
-if __name__ == '__main__':
+def run():
     'parse args to run functions'
     # global options
-    parser = argparse.ArgumentParser(description='Manage text-based task list from the command line.')
-    parser.add_argument('-c', '--color', default='y', choices=['y', 'n'], help="Colorized Output")
+    parser = argparse.ArgumentParser(
+        description='Manage text-based task list from the command line.'
+    )
+    parser.add_argument(
+        '-c', '--color', 
+        default='y', choices=['y', 'n'], 
+        help="Colorized Output"
+    )
 
     subparsers = parser.add_subparsers()
 
@@ -124,7 +130,10 @@ if __name__ == '__main__':
 
     # complete
     complete_parser = subparsers.add_parser('complete', help='complete a task')
-    complete_parser.add_argument('n', type=int, help='The task number to mark as completed')
+    complete_parser.add_argument(
+        'n', type=int, 
+        help='The task number to mark as completed'
+    )
     complete_parser.set_defaults(func=complete_todo)
 
     # search
@@ -132,9 +141,5 @@ if __name__ == '__main__':
     search_parser.add_argument('text', help='The text of your search')
     search_parser.set_defaults(func=search_todos)
 
-    # projects
-    projects_parser = subparsers.add_parser('projects', help='view a list of projects, or modify them.')
-    projects_parser.set_defaults(func=refresh_projects)
-
-    args = parser.parse_args()
-    args.func(args)
+if __name__ == '__main__':
+    run()
