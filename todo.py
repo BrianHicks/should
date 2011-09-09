4b6f0358ec7551c43ee748b3ceb668a6ac970f6f
Brian Hicks <brian.hicks@rockfishinteractive.com>
2011-09-09 11:47:35 -0500

    add color, better commenting

--- todo.py
+++ todo.py
@@ -1,9 +1,42 @@
-'methods for working with plain text todo lists'
+'Should, a small command line app for working with plain text todo files'
 import argparse
 import os
 import re
 import sys
 from datetime import datetime
+
+## COLORS ##
+
+COLORS = {
+    'grey': '\033[1;30m',
+    'red': '\033[1;31m',
+    'green': '\033[1;32m',
+    'yellow': '\033[1;33m',
+    'blue': '\033[1;34m',
+    'magenta': '\033[1;35m',
+    'cyan': '\033[1;36m',
+    'white': '\033[1;37m',
+    'hred': '\033[1;41m',
+    'hgreen': '\033[1;42m',
+    'hbrown': '\033[1;43m',
+    'hblue': '\033[1;44m',
+    'hmagenta': '\033[1;45m',
+    'hcyan': '\033[1;46m',
+    'hgrey': '\033[1;47m',
+    'hcrimson': '\033[1;48m',
+    'end': '\033[1;m',
+}
+
+def wrap_color(text, *args):
+    for arg in args:
+        try:
+            text = COLORS[arg] + text + COLORS['end']
+        except KeyError:
+            pass
+
+    return text
+
+## METHODS ##
 
 def get_absolute_file_name(name):
     return os.path.join(os.path.dirname(__file__), '%s.txt' % name)
@@ -26,10 +59,13 @@ def write_named_file_lines(name, lines):
         print 'could not write lines to file, here they are:'
         print lines
 
+def format_verbose_line(i, text):
+    return '%s: %s' % (wrap_color(str(i), 'blue'), text)
+
 def print_todos():
     todos = get_named_file_lines('todo')
-    for todo in enumerate(todos):
-        print '%s: %s' % todo
+    for i, todo in enumerate(todos):
+        print format_verbose_line(i, todo)
 
 def complete_todo(n):
     todos = get_named_file_lines('todo')
@@ -57,7 +93,7 @@ def search_todo(text):
     todos = get_named_file_lines('todo')
     for i, todo in enumerate(todos):
         if todo.find(text) != -1:
-            print '%d: %s' % (i, todo)
+            print format_verbose_line(i, todo)
 
 def refresh_projects():
     print 'not fully functional.'
@@ -65,6 +101,8 @@ def refresh_projects():
     projects = {}
     for todo in todos:
         print todo
+
+## ARGUMENT PARSING ##
 
 def parse(args):
     if args.command in ['show', 's']:
@@ -77,7 +115,7 @@ def parse(args):
         search_todo(args.m)
     elif args.command in ['projects', 'p']:
         refresh_projects()
-        
+
 parser = argparse.ArgumentParser(description='Manage todos.')
 parser.add_argument(
     'command', metavar='cmd', type=str, 
